e4728d898b164a8b914ca0002a52d7b5ab9697be
Veronika Rozová <160782720+VeruRoz@users.noreply.github.com>
2025-06-08 18:13:37 +0200

    Projekt Hungry Horace

---
+++ WPF-Hungry_Horace/Level.xaml.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPF_Hungry_Horace
+{
+    /// <summary>
+    /// Interaction logic for Level.xaml
+    /// </summary>
+    /// 
+
+
+    public partial class Level : UserControl
+    {
+        private int horaceRow = 8;
+        private int horaceCol = 1;
+
+        private int _skore;
+        public int Skore
+        {
+            get => _skore;
+            set
+            {
+                _skore = value;
+                SkoreChanged?.Invoke(this, _skore);
+            }
+        }
+
+        public event EventHandler<int> SkoreChanged;
+
+        private DispatcherTimer priserkaTimer;
+
+
+
+        public Level()
+        {
+            InitializeComponent();
+            this.Loaded += Level_Loaded;
+            Skore = 0;
+
+            priserkaTimer = new DispatcherTimer();
+            priserkaTimer.Interval = TimeSpan.FromMilliseconds(500);
+            priserkaTimer.Tick += (s, e) => Monster_Move();
+            priserkaTimer.Start();
+        }
+
+        private void Level_Loaded(object sender, RoutedEventArgs e)
+        {
+            this.Focusable = true;
+            this.Focus();
+            this.KeyDown += Level_KeyDown;
+        }
+
+        private Random rng = new Random();
+
+        private void Monster_Move()
+        {
+            int radek = Grid.GetRow(priserka);
+            int sloupec = Grid.GetColumn(priserka);
+
+            var smery = new List<(int dRow, int dCol)>
+            {
+        (-1,0), (1,0), (0,-1), (0,1)
+             };
+
+
+            smery = smery.OrderBy(x => rng.Next()).ToList();
+
+            foreach (var smer in smery)
+            {
+                int newRow = radek + smer.dRow;
+                int newCol = sloupec + smer.dCol;
+
+                bool Wall = IsThereWall(newRow, newCol);
+
+                int HoraceRow = Grid.GetRow(Horace);
+                int HoraceCol = Grid.GetColumn(Horace);
+                if (newRow == HoraceRow && newCol == HoraceCol)
+                {
+                    Horace.Visibility = Visibility.Hidden;
+                    Skore = -1;
+
+                }
+
+                if (!Wall)
+                {
+                    Grid.SetRow(priserka, newRow);
+                    Grid.SetColumn(priserka, newCol);
+                    break;
+                }
+                
+            }
+        }
+
+
+        private void Level_KeyDown(object sender, KeyEventArgs e)
+        {
+            int newRow = horaceRow;
+            int newCol = horaceCol;
+
+
+
+            switch (e.Key)
+            {
+                case Key.Up:
+                    newRow--;
+                    break;
+                case Key.Down:
+                    newRow++;
+                    break;
+                case Key.Left:
+                    newCol--;
+                    break;
+                case Key.Right:
+                    newCol++;
+                    break;
+            }
+
+
+
+            //okraje + vnitřní stěny
+            if (newRow >= 1 && newRow < 9 && newCol >= 1 && newCol < 9)
+            {
+                foreach (var shape in GameBoard.Children.OfType<Shape>())
+                {
+                    if ((string)shape.Tag == "wall")
+                    {
+                        int row = Grid.GetRow(shape);
+                        int col = Grid.GetColumn(shape);
+
+                        if (row == newRow && col == newCol)
+                        {
+                            newCol = horaceCol;
+                            newRow = horaceRow;
+                        }
+                    }
+
+
+                }
+                //je na příšrce?
+                int priserkaRow = Grid.GetRow(priserka);
+                int priserkaCol = Grid.GetColumn(priserka);
+                if (horaceRow == priserkaRow && horaceCol == priserkaCol)
+                {
+                    Horace.Visibility = Visibility.Collapsed;
+                    Skore = -1;
+
+                }
+
+                horaceRow = newRow;
+                horaceCol = newCol;
+
+                Grid.SetRow(Horace, horaceRow);
+                Grid.SetColumn(Horace, horaceCol);
+            }
+            foreach (var shape in GameBoard.Children.OfType<Shape>())
+            {
+
+                if ((string)shape.Tag == "food")
+                {
+                    //když jsou na stejných souzadnicích tak jidlo zmizí a navýší skore
+
+                    int row = Grid.GetRow(shape);
+                    int col = Grid.GetColumn(shape);
+
+                    if (row == horaceRow && col == horaceCol && shape.Visibility == Visibility.Visible && Horace.Visibility == Visibility.Visible)
+                    {
+                        shape.Visibility = Visibility.Hidden;
+                        Skore++;
+                    }
+                }
+            }
+
+
+        }
+
+
+        private bool IsThereWall(int newRow, int newCol)
+        {
+            // Mimo herní plochu se chová jako zeď
+            if (newRow < 1 || newRow > 8 || newCol < 1 || newCol > 8)
+                return true;
+
+            foreach (var shape in GameBoard.Children.OfType<Shape>())
+            {
+                if (shape.Tag is string tag && tag == "wall")
+                {
+                    int row = Grid.GetRow(shape);
+                    int col = Grid.GetColumn(shape);
+
+                    if (row == newRow && col == newCol)
+                    {
+                        return true;
+                    }
+                }
+            }
+
+            return false;
+        }
+    }
+}
---
+++ WPF-Hungry_Horace/MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System.Diagnostics;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_Hungry_Horace;
+enum GameStage { Intro, Game, OutroWin, OutroLosse }
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    private GameStage Stage;
+    private Level LevelControl;
+    public MainWindow()
+    {
+        InitializeComponent();
+        Stage = GameStage.Intro;
+
+        LevelControl = new Level();
+        LevelControl.SkoreChanged += LevelControl_SkoreChanged;
+
+        Grid.SetRow(LevelControl, 1);
+        Grid.SetColumn(LevelControl, 1);
+        GameLevel_1.Children.Add(LevelControl);
+    }
+    private void LevelControl_SkoreChanged(object sender, int noveSkore)
+    {
+        if (Stage == GameStage.Game)
+        {
+            if (noveSkore == 6)
+            {
+                Stage = GameStage.OutroWin;
+                TogglePanels();
+            }
+            else if (noveSkore < 0)
+            {
+                Stage = GameStage.OutroLosse;
+                TogglePanels();
+            }
+        }
+
+        Skore.Text = noveSkore.ToString();
+
+    }
+
+    private void StartBTN_Click(object sender, RoutedEventArgs e)
+    {
+        Stage = GameStage.Game;
+        TogglePanels();
+    }
+
+    private void TogglePanels()
+    {
+        Start.Visibility = Visibility.Hidden;
+        GameLevel_1.Visibility = Visibility.Hidden;
+        Win.Visibility = Visibility.Hidden;
+        lost.Visibility = Visibility.Hidden;
+
+        switch (Stage)
+        {
+            case GameStage.Intro:
+                Start.Visibility = Visibility.Visible;
+                break;
+
+            case GameStage.Game:
+                GameLevel_1.Visibility = Visibility.Visible;
+                LevelControl.Focusable = true;
+                LevelControl.Focus();
+                break;
+
+            case GameStage.OutroWin:
+                Win.Visibility = Visibility.Visible;
+                break;
+
+            case GameStage.OutroLosse:
+                lost.Visibility = Visibility.Visible;
+                break;
+        }
+    }
+}
+
+    
